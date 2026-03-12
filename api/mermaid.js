db0f8f4369b8e29f464f26186d3eef8d76300773
Syed Wasif Shah <wasifatwork@gmail.com>
2026-03-12 08:58:22 +0500

    Update mermaid.js

--- api/mermaid.js
+++ api/mermaid.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   try {
     const { JSDOM } = await import("jsdom");
     const dom = new JSDOM("<!DOCTYPE html>");
-    
+
     global.window = dom.window;
     global.document = dom.window.document;
     global.DOMParser = dom.window.DOMParser;
@@ -16,12 +16,23 @@ export default async function handler(req, res) {
       isSupported: true,
     };
 
+    // Mock SVG getBBox which jsdom doesn't support
+    dom.window.SVGElement.prototype.getBBox = () => ({
+      x: 0, y: 0, width: 100, height: 50
+    });
+    dom.window.SVGElement.prototype.getBoundingClientRect = () => ({
+      x: 0, y: 0, width: 100, height: 50, top: 0, left: 0, bottom: 50, right: 100
+    });
+    dom.window.Element.prototype.getBBox = () => ({
+      x: 0, y: 0, width: 100, height: 50
+    });
+
     const { parseMermaidToExcalidraw } = await import("@excalidraw/mermaid-to-excalidraw");
-    
+
     const { mermaid } = req.body;
     const { elements } = await parseMermaidToExcalidraw(mermaid);
 
-    return res.status(200).json({ 
+    return res.status(200).json({
       success: true,
       elementCount: elements?.length
     });
