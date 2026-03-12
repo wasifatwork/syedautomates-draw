ec247ce9a04d4a99b8b71b693722e7c0ea353b61
Syed Wasif Shah <wasifatwork@gmail.com>
2026-03-12 08:30:39 +0500

    Update mermaid.js

--- api/mermaid.js
+++ api/mermaid.js
@@ -1,3 +1,13 @@
+import { JSDOM } from "jsdom";
+import createDOMPurify from "isomorphic-dompurify";
+
+// Polyfill browser globals before mermaid loads
+const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>");
+global.window = dom.window;
+global.document = dom.window.document;
+global.DOMParser = dom.window.DOMParser;
+global.DOMPurify = createDOMPurify(dom.window);
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -7,7 +17,6 @@ export default async function handler(req, res) {
     const { mermaid } = req.body;
 
     const { parseMermaidToExcalidraw } = await import("@excalidraw/mermaid-to-excalidraw");
-
     const result = await parseMermaidToExcalidraw(mermaid);
 
     return res.status(200).json({ 
